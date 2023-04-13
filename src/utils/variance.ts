90613f2f595b4a969777bed353d0ebb2079525fc
pewpewnor <newpatchnotworking2@tutanota.com>
2023-04-13 19:26:53 +0700

    refactor: refactor variance utility tool

--- src/utils/variance.ts
+++ src/utils/variance.ts
@@ -1,63 +1,63 @@
 import { twMerge } from "tailwind-merge";
 
-interface VariantsParameter {
-	[key: string]: {
-		default: string;
-		[key: string]: string;
-	};
-}
-
-interface SelectedVariantParameter {
+interface Variant {
+	default: string;
 	[key: string]: string;
 }
 
-export default function createVariance(
-	always: string,
-	variants: VariantsParameter
-): (
-	selectedVariant: SelectedVariantParameter,
-	customClassName?: string
-) => string {
-	return (
-		selectedVariant: SelectedVariantParameter,
-		customClassName?: string
-	): string => {
-		const result: string[] = [always];
+interface CreatedVariants {
+	[key: string]: Variant;
+}
 
-		// catch error if any specified variant type does not exist
-		for (const selectedVariantType in selectedVariant) {
-			if (!variants[selectedVariantType]) {
+interface ChosenVariants {
+	[key: string]: string;
+}
+
+class Variance {
+	private always: string;
+	private createdVariants: CreatedVariants;
+
+	constructor(always: string, createdVariants: CreatedVariants) {
+		this.always = always;
+		this.createdVariants = createdVariants;
+	}
+
+	public getVariant(chosenVariants: ChosenVariants) {
+		// Throw error if any of the chosen variant type does not exist in created variants
+
+		Object.keys(chosenVariants).forEach((chosenVariantType) => {
+			if (!Object.hasOwn(this.createdVariants, chosenVariantType)) {
 				throw new Error(
-					`From variance util -> Selected variant type "${selectedVariantType}" does not exist!`
+					`Chosen variant type "${chosenVariantType}" does not exist!`
 				);
 			}
-		}
+		});
 
-		// loop for each variants available
-		for (const variantType in variants) {
-			// if this variant type is specified in selected
-			if (variantType in selectedVariant) {
-				const variant =
-					variants[variantType][selectedVariant[variantType]];
-				// if specified value for this variant type is invalid, throw error
-				if (!variant) {
-					throw new Error(
-						`From variance util -> Selected variant "${selectedVariant[variantType]}" does not exist in variant type "${variantType}"!`
-					);
+		const result = [
+			this.always,
+			// Loop for each created variants
+
+			...Object.entries(this.createdVariants).map(
+				([variantType, variant]) => {
+					// If user specify this variant type
+
+					if (Object.hasOwn(chosenVariants, variantType)) {
+						const chosenVariantKey = chosenVariants[variantType];
+						if (Object.hasOwn(variant, chosenVariantKey)) {
+							return variant[chosenVariantKey];
+						} else {
+							throw new Error(
+								`Chosen variant "${chosenVariantKey}" does not exist in variant type "${variantType}"!`
+							);
+						}
+					}
+					// If user did not specify this variant type, use the default property value
+
+					return variant.default;
 				}
-				result.push(variant);
-			}
-			// if this variant type is not specified, use the default property value
-			else {
-				result.push(variants[variantType]["default"]);
-			}
-		}
-
-		// if custom className is specified
-		if (customClassName) {
-			result.push(customClassName);
-		}
+			),
+		];
 
 		return twMerge(result.join(" "));
-	};
+	}
 }
