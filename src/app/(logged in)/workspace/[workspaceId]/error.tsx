bf1e881d8a6f9e7819dfe9cb6043092a9288e349
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-04-22 21:30:17 +0700

    feat: add error page

---
+++ src/app/(logged in)/workspace/[workspaceId]/error.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { FC } from "react";
+
+interface ErrorProps {}
+
+const Error: FC<ErrorProps> = (props: ErrorProps) => {
+	return <div>Error</div>;
+};
+
+export default Error;
