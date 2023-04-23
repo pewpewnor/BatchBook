b21be055a77e83722ee2ed786469f8269931b1a2
pewpewnor <newpatchnotworking2@tutanota.com>
2023-04-23 18:23:21 +0700

    refactor: better error message format

--- src/utils/error-message.ts
+++ src/utils/error-message.ts
@@ -1,7 +1,10 @@
 export default function createErrorMessage(
 	location: string,
 	functionName: string,
-	errorMessage: string
+	errorMessage: string,
+	...causes: string[]
 ) {
-	return `[${location}]\n\t[${functionName}]\n\t\t${errorMessage}`;
+	return `[location: ${location}]\n\t[function name: ${functionName}]\n\t\t${errorMessage}\n\t\t${causes.join(
+		"\n\t\t"
+	)}`;
 }
