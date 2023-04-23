e1ccddcedbd2376bbed6736f374606a7afab8560
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-04-23 19:13:11 +0700

    feat: adjust workspacedata type accordingly

--- src/lib/database/workspace-type.ts
+++ src/lib/database/workspace-type.ts
@@ -1,5 +1,26 @@
 import { Prisma } from "@prisma/client";
 
+type WorkspaceData = Prisma.WorkspaceGetPayload<{
+	include: {
+		members: true;
+		channelSections: {
+			select: {
+				id: true;
+				name: true;
+				channels: {
+					select: {
+						id: true;
+						name: true;
+						type: true;
+						BoardChannel: true;
+						ThreadChannel: true;
+					};
+				};
+			};
+		};
+	};
+}>;
+
 type ChannelSectionData = Prisma.ChannelSectionGetPayload<{
 	select: {
 		id: true;
@@ -8,17 +29,14 @@ type ChannelSectionData = Prisma.ChannelSectionGetPayload<{
 	};
 }>;
 
-type WorkspaceData = Prisma.WorkspaceGetPayload<{
-	include: {
-		members: true;
-		channelSections: {
-			select: {
-				id: true;
-				name: true;
-				channels: true;
-			};
-		};
+type ChannelData = Prisma.ChannelGetPayload<{
+	select: {
+		id: true;
+		name: true;
+		type: true;
+		BoardChannel: true;
+		ThreadChannel: true;
 	};
 }>;
 
-export type { WorkspaceData, ChannelSectionData };
+export type { WorkspaceData, ChannelSectionData, ChannelData };
