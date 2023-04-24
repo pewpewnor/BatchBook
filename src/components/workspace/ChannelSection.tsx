9f3fb107cdfbbddbe6d9d3df4df00dc4e42f96a1
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-04-24 15:36:37 +0700

    feat: integrate channel component

--- src/components/workspace/ChannelSection.tsx
+++ src/components/workspace/ChannelSection.tsx
@@ -1,19 +1,48 @@
 "use client";
+import Channel from "@/components/workspace/Channel";
 import { ChannelSectionData } from "@/lib/database/workspace-type";
-import { FC } from "react";
+import { FC, useState } from "react";
+import { BsCaretDown, BsCaretDownFill, BsPlusLg } from "react-icons/bs";
 
 interface ChannelSectionProps extends ChannelSectionData {}
 
 const ChannelSection: FC<ChannelSectionProps> = (
 	props: ChannelSectionProps
 ) => {
+	const [dropdown, setDropdown] = useState<boolean>(true);
+
+	function handleDropdown() {
+		setDropdown((prev) => !prev);
+	}
+
 	return (
-		<div className="bg-shade-blue flex justify-between">
-			{/* Left section */}
-			<div className="flex">{props.name}</div>
-			{/* Right section */}
-			<div className="flex">+</div>
-		</div>
+		<>
+			<div className="bg-shade-blue flex justify-between pl-2 pr-4 my-2">
+				{/* Left section */}
+				<div
+					className="flex gap-x-2 items-center group"
+					onClick={handleDropdown}
+				>
+					{dropdown ? <BsCaretDownFill /> : <BsCaretDown />}
+					<p className="text-sm group-hover:font-bold">
+						{props.name}
+					</p>
+				</div>
+				{/* Right section */}
+				<div className="flex items-center gap-x-2">
+					<BsPlusLg />
+				</div>
+			</div>
+
+			{/* Dropdown channels that belongs to this channel section */}
+			{dropdown && (
+				<div className="flex flex-col">
+					{props.channels.map((channel) => (
+						<Channel key={channel.id} {...channel} />
+					))}
+				</div>
+			)}
+		</>
 	);
 };
 
