132b3d9d748ce89b00f98b776094bc2ce28b6f28
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-04-24 18:37:41 +0700

    style: fix small topbwhite gap

--- src/components/workspace/Channel.tsx
+++ src/components/workspace/Channel.tsx
@@ -9,7 +9,7 @@ const Channel: FC<ChannelProps> = (props: ChannelProps) => {
 	return (
 		<div className="mx-2 mb-1 flex justify-between rounded-md bg-shade-blue py-1 pl-6 pr-2 hover:bg-light-shade-blue">
 			{/* Left section */}
-			<div className="flex items-center gap-x-2">
+			<div className="flex items-center gap-x-2 pr-2">
 				<HiHashtag />
 				{props.name}
 			</div>
