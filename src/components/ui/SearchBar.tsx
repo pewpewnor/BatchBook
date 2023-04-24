3d0aa275f743ccc5bb88fff5efbb3b72803d3bd5
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-04-24 18:45:43 +0700

    style: fix text overlap

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -11,9 +11,9 @@ const SearchBar: FC<SearchBarProps> = (props: SearchBarProps) => {
 			<input
 				type="text"
 				placeholder="Search ..."
-				className="hidden h-9 w-[30vw] rounded-2xl bg-shade-blue px-5 sm:block md:w-[20vw]"
+				className="hidden h-9 w-[30vw] rounded-2xl bg-shade-blue px-5 pr-12 sm:block md:w-[20vw]"
 			/>
-			<div className="flex cursor-pointer items-center px-3 sm:absolute sm:inset-y-0 sm:right-1 sm:rounded-r-2xl">
+			<div className="flex cursor-pointer items-center px-3 hover:bg-black sm:absolute sm:inset-y-0 sm:right-0 sm:rounded-r-2xl">
 				<GoSearch className="h-5 w-5 fill-white" />
 			</div>
 		</div>
