fd473d5332eb6f8ca07992cfd8ea077bfbcb946d
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-22 18:48:33 +0700

    style: remove same background color

--- src/components/workspace/Channel.tsx
+++ src/components/workspace/Channel.tsx
@@ -7,7 +7,7 @@ interface ChannelProps extends ChannelData {}
 
 const Channel: FC<ChannelProps> = (props: ChannelProps) => {
 	return (
-		<div className="mx-2 mb-1 flex justify-between rounded-md bg-shade-blue py-1 pl-6 pr-2 hover:bg-light-shade-blue">
+		<div className="mx-2 mb-1 flex justify-between rounded-md py-1 pl-6 pr-2 hover:bg-light-shade-blue">
 			{/* Left section */}
 			<div className="flex items-center gap-x-2 pr-2">
 				<HiHashtag />
