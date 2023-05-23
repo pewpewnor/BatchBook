3a393ca97cd2df4e059772d43fa46e3e4735024e
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-23 14:57:56 +0700

    feat: add thread page template

---
+++ src/components/threadchannel/ThreadPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface ThreadPageProps {}
+
+const ThreadPage: FC<ThreadPageProps> = (props: ThreadPageProps) => {
+	return <div>ThreadPage</div>;
+};
+
+export default ThreadPage;
