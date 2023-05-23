d3849d8fd19eff285b9e833f4d999ed76e61c959
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-23 19:00:50 +0700

    feat: add switch channel foundation

--- src/app/(logged in)/workspace/[workspaceId]/page.tsx
+++ src/app/(logged in)/workspace/[workspaceId]/page.tsx
@@ -1,7 +1,10 @@
 "use client";
+import BoardPage from "@/components/boardpage/BoardPage";
+import ThreadPage from "@/components/threadpage/ThreadPage";
 import LoadingSpinner from "@/components/ui/LoadingSpinner";
 import ChannelSection from "@/components/workspace/ChannelSection";
-import { WorkspaceData } from "@/lib/database/workspace-type";
+import SwitchChannelContext from "@/contexts/SwitchChannelContext";
+import { ChannelData, WorkspaceData } from "@/types/workspace-type";
 import createErrorMessage from "@/utils/error-message";
 import { notFound } from "next/navigation";
 import { FC, useState } from "react";
@@ -9,14 +12,17 @@ import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
 import { useQuery } from "react-query";
 import { assert } from "ts-essentials";
 
-interface WorkspaceProps {
+interface WorkspacePageProps {
 	params: {
 		workspaceId: string;
 	};
 }
 
-const Workspace: FC<WorkspaceProps> = (props: WorkspaceProps) => {
+const WorkspacePage: FC<WorkspacePageProps> = (props: WorkspacePageProps) => {
 	const [isChannelSidebarOpen, setIsChannelSidebarOpen] = useState(true);
+	const [currentChannel, setCurrentChannel] = useState<ChannelData | null>(
+		null
+	);
 
 	const workspaceQuery = useQuery<WorkspaceData | null>(
 		["workspace", props.params.workspaceId],
@@ -44,10 +50,12 @@ const Workspace: FC<WorkspaceProps> = (props: WorkspaceProps) => {
 			}
 
 			const data = await response.json();
+
 			assert(
 				data === null || (data as WorkspaceData),
 				"returned data from API must be either be null or compatible with type WorkspaceData"
 			);
+
 			return data as WorkspaceData | null;
 		},
 		{
@@ -84,8 +92,15 @@ const Workspace: FC<WorkspaceProps> = (props: WorkspaceProps) => {
 		notFound();
 	}
 
+	if (!currentChannel) {
+		const firstChannel = workspaceQuery.data.channelSections[0].channels[0];
+		if (firstChannel) {
+			setCurrentChannel(firstChannel);
+		}
+	}
+
 	return (
-		<div className="relative flex h-full flex-row">
+		<div className="relative flex h-full">
 			{/* Channel Sidebar */}
 			<div
 				className={`relative h-full border-0 bg-shade-blue bg-opacity-75 pt-1 shadow-lg ${
@@ -93,16 +108,20 @@ const Workspace: FC<WorkspaceProps> = (props: WorkspaceProps) => {
 				}`}
 			>
 				<div className={isChannelSidebarOpen ? "block" : "hidden"}>
-					{workspaceQuery.data.channelSections.map(
-						(channelSection) => {
-							return (
-								<ChannelSection
-									key={channelSection.id}
-									{...channelSection}
-								/>
-							);
-						}
-					)}
+					<SwitchChannelContext.Provider
+						value={[currentChannel, setCurrentChannel]}
+					>
+						{workspaceQuery.data.channelSections.map(
+							(channelSection) => {
+								return (
+									<ChannelSection
+										key={channelSection.id}
+										{...channelSection}
+									/>
+								);
+							}
+						)}
+					</SwitchChannelContext.Provider>
 				</div>
 
 				{isChannelSidebarOpen ? (
@@ -125,11 +144,28 @@ const Workspace: FC<WorkspaceProps> = (props: WorkspaceProps) => {
 			</div>
 
 			{/* Main content */}
-			<div className="h-full">
-				<pre>{JSON.stringify(workspaceQuery.data, null, 2)}</pre>
+			<div className="h-full w-full">
+				{!currentChannel ? (
+					<div className="flex h-full items-center justify-center">
+						<p className="text-lg">
+							Let&apos;s pick a channel to open!
+						</p>
+					</div>
+				) : currentChannel.boardChannel ? (
+					<BoardPage />
+				) : currentChannel.threadChannel ? (
+					<ThreadPage />
+				) : (
+					<div className="flex h-full items-center justify-center">
+						<p className="text-lg">
+							This channel seems to not work properly
+						</p>
+					</div>
+				)}
+				{/* <pre>{JSON.stringify(workspaceQuery.data, null, 2)}</pre> */}
 			</div>
 		</div>
 	);
 };
 
-export default Workspace;
+export default WorkspacePage;
