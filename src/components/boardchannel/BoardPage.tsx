5d75a51c794e419076d016289195d07b104c9bdf
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-23 14:57:46 +0700

    feat: add board page template

---
+++ src/components/boardchannel/BoardPage.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface BoardPageProps {}
+
+const BoardPage: FC<BoardPageProps> = (props: BoardPageProps) => {
+	return <div>BoardPage</div>;
+};
+
+export default BoardPage;
