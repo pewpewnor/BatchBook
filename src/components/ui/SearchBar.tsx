02248bd80852ed8d1fa6af9629983392946f3ebb
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-24 13:20:11 +0700

    style: remove pointer cursor

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -13,7 +13,7 @@ const SearchBar: FC<SearchBarProps> = (props: SearchBarProps) => {
 				placeholder="Search ..."
 				className="hidden h-8 w-[30vw] rounded-2xl bg-shade-blue px-5 pr-12 sm:block md:w-[20vw]"
 			/>
-			<div className="flex cursor-pointer items-center px-3 sm:absolute sm:inset-y-0 sm:right-0 sm:rounded-r-2xl">
+			<div className="flex items-center px-3 sm:absolute sm:inset-y-0 sm:right-0 sm:rounded-r-2xl">
 				<BsSearch className="h-5 w-5 fill-white" />
 			</div>
 		</div>
