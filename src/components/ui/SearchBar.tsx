6bd014928d79e2e072f189cf657a0e13c9f86303
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-24 16:01:25 +0700

    feat: add workbar to board view

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -7,14 +7,14 @@ interface SearchBarProps {}
 
 const SearchBar: FC<SearchBarProps> = (props: SearchBarProps) => {
 	return (
-		<div className="flex justify-between sm:relative">
+		<div className="relative flex justify-between">
 			<input
 				type="text"
 				placeholder="Search ..."
-				className="hidden h-8 w-[30vw] rounded-2xl bg-shade-blue px-5 pr-12 sm:block md:w-[20vw]"
+				className="h-[1.8rem] w-[30vw] rounded-2xl bg-white/30 px-5 pr-12 backdrop-blur-lg"
 			/>
-			<div className="flex items-center px-3 sm:absolute sm:inset-y-0 sm:right-0 sm:rounded-r-2xl">
-				<BsSearch className="h-5 w-5 fill-white" />
+			<div className="absolute inset-y-0 right-0 flex items-center rounded-r-2xl px-3">
+				<BsSearch className="h-4 w-4 fill-white" />
 			</div>
 		</div>
 	);
---
+++ src/page/workspace/WorkBar.tsx
@@ -0,0 +1,19 @@
+import SearchBar from "@/components/ui/SearchBar";
+import { FC } from "react";
+import { BsFillPlusSquareFill } from "react-icons/bs";
+
+interface WorkBarProps {}
+
+const WorkBar: FC<WorkBarProps> = (props: WorkBarProps) => {
+	return (
+		<div className="flex h-12 w-full items-center justify-around">
+			<SearchBar />
+			<BsFillPlusSquareFill className="mr-2 h-6 w-6 fill-slate-400" />
+			<button className="rounded-lg bg-white/30 px-2 py-1 text-sm backdrop-blur-xl">
+				Invite
+			</button>
+		</div>
+	);
+};
+
+export default WorkBar;
