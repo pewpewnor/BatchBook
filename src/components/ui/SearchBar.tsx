84a9c0a1cfa27af589db8e9421281e6aed3bde4b
pewpewnor <newpatchnotworking2@tutanota.com>
2023-05-24 13:16:01 +0700

    style: change search icon

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FC } from "react";
-import { GoSearch } from "react-icons/go";
+import { BsSearch } from "react-icons/bs";
 
 interface SearchBarProps {}
 
@@ -13,8 +13,8 @@ const SearchBar: FC<SearchBarProps> = (props: SearchBarProps) => {
 				placeholder="Search ..."
 				className="hidden h-8 w-[30vw] rounded-2xl bg-shade-blue px-5 pr-12 sm:block md:w-[20vw]"
 			/>
-			<div className="flex cursor-pointer items-center px-3 hover:bg-light-shade-blue sm:absolute sm:inset-y-0 sm:right-0 sm:rounded-r-2xl">
-				<GoSearch className="h-5 w-5 fill-white" />
+			<div className="flex cursor-pointer items-center px-3 sm:absolute sm:inset-y-0 sm:right-0 sm:rounded-r-2xl">
+				<BsSearch className="h-5 w-5 fill-white" />
 			</div>
 		</div>
 	);
