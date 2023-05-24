8b28f59eb452122e47e5e10e8413b2be20a181c6
pewpewnor <125549982+pewpewnor@users.noreply.github.com>
2023-05-24 12:59:40 +0700

    style: reduce navbar height

--- src/components/ui/SearchBar.tsx
+++ src/components/ui/SearchBar.tsx
@@ -11,7 +11,7 @@ const SearchBar: FC<SearchBarProps> = (props: SearchBarProps) => {
 			<input
 				type="text"
 				placeholder="Search ..."
-				className="hidden h-9 w-[30vw] rounded-2xl bg-shade-blue px-5 pr-12 sm:block md:w-[20vw]"
+				className="hidden h-8 w-[30vw] rounded-2xl bg-shade-blue px-5 pr-12 sm:block md:w-[20vw]"
 			/>
 			<div className="flex cursor-pointer items-center px-3 hover:bg-light-shade-blue sm:absolute sm:inset-y-0 sm:right-0 sm:rounded-r-2xl">
 				<GoSearch className="h-5 w-5 fill-white" />
